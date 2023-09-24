994944f9aecc129b5c447705c268a5624c0ae88d
Nested Software <2969361+nestedsoftware@users.noreply.github.com>
2023-09-23 22:13:11 -0600

    let id autoincrement

--- springboot-app/src/main/resources/data.sql
+++ springboot-app/src/main/resources/data.sql
@@ -3,8 +3,8 @@ id INT AUTO_INCREMENT  PRIMARY KEY,
 name VARCHAR(50) NOT NULL
 );
 
-INSERT INTO items (id, name) VALUES (1, 'coffee');
-INSERT INTO items (id, name) VALUES (2, 'tea');
-INSERT INTO items (id, name) VALUES (3, 'cookies');
-INSERT INTO items (id, name) VALUES (4, 'cupcakes');
-INSERT INTO items (id, name) VALUES (5, 'muffins');
+INSERT INTO items (name) VALUES ('coffee');
+INSERT INTO items (name) VALUES ('tea');
+INSERT INTO items (name) VALUES ('cookies');
+INSERT INTO items (name) VALUES ('cupcakes');
+INSERT INTO items (name) VALUES ('muffins');
